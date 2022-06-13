744053e8fd7786410ba8a1da96a903c4eb07c1b4
stecarraa <stefano@caratozzolo.me>
2022-06-13 17:01:05 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+//definition of variables
+
+//prompt for the name
+const userName = prompt("Insert your name");
+
+//promt for surname
+const userSurname = prompt("Insert your surname");
+
+//prompt for fav colour
+const FavouriteColour = prompt("Insert your favourite colour");
+
+
+//stamp
+document.
+getElementById('password').
+innerHTML = `The most secure passw I could think of is:          
+${userName + userSurname + FavouriteColour}22`
